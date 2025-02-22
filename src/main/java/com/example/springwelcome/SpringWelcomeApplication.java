e57e85a38ade3ae223e77d2c188ece6640aa4a43
Shantanu Patil <s.patil@bm-systems.in>
2025-02-22 13:08:31 +0530

    intial commit

---
+++ src/main/java/com/example/springwelcome/SpringWelcomeApplication.java
@@ -0,0 +1,11 @@
+package com.example.springwelcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWelcomeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWelcomeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/springwelcome/SpringWelcomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwelcome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWelcomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
